190d0afff480795c1c115a169e3a6a446ceb734b
Chris Raidler <chris@raidler.ch>
2020-02-12 18:41:30 +0100

    Module improvement

--- module/Logger/bin/logger.php
+++ module/Logger/bin/logger.php
@@ -9,4 +9,5 @@ $errno = null;
 $errstr = null;
 $stream = stream_socket_client('tcp://localhost:2947',$errno ,$errstr , 10);
 stream_set_blocking($stream, 1);
+fwrite($stream, '?WATCH={"enable":true,"json":true}');
 while (true) var_dump(fgets($stream));
