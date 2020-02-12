1f93a9c6451a0ad123eecc7325c89192fd1dfef5
Chris Raidler <chris@raidler.ch>
2020-02-12 18:39:47 +0100

    Module improvement

--- module/Logger/bin/logger.php
+++ module/Logger/bin/logger.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 # todo: this script could use some love
 
-use DateTime;
-
-var_dump(file_get_contents('tcp://localhost:2947'));
+$errno = null;
+$errstr = null;
+$stream = stream_socket_client('tcp://localhost:2947',$errno ,$errstr , 10);
+stream_set_blocking($stream, 1);
+while (true) var_dump(fgets($this->stream));
