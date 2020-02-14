c3bc171d2f30574336779e8432dbcf2da6ad9af9
Chris Raidler <chris@raidler.ch>
2020-02-14 20:08:42 +0100

    Bugfixes

--- module/Logger/bin/gpsl.php
+++ module/Logger/bin/gpsl.php
@@ -2,6 +2,7 @@
 namespace Logger;
 
 use Laminas\Config\Config;
+use Logger\Model\Nmea\Log;
 use Logger\Service\Nmea;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
@@ -10,7 +11,7 @@ $config = (new Config(include __DIR__ . '/../config/module.config.php'))->get(Mo
 $service = new Nmea($config->get('nmea'));
 
 do {
-    $item = $service->collect();
+    $item = Log::createFromItem($service->collect());
     $output = fopen(sprintf('%s/%s.dat', $config->get('file')->get('path'), date('Ymd')), 'a');
     fwrite($output, sprintf('%s' . PHP_EOL, implode(',', $item->getArrayCopy())));
     fclose($output);
