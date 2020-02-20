2915cc32c6b8e8706582215f9a9fecc9e1950cff
Chris Raidler <chris@raidler.ch>
2020-02-20 10:10:57 +0100

    Decimaldegree to sexagesimal

--- module/Application/src/Util/Coordinates.php
+++ module/Application/src/Util/Coordinates.php
@@ -10,25 +10,38 @@ abstract class Coordinates
     const LATITUDE = 0;
     const LONGITUDE = 1;
 
-    public static function dmsToDec(string $dms): float
+    /**
+     * Sexagesimal to decimaldegree
+     * @param string $dms
+     * @param string $hdg
+     * @return float
+     */
+    public static function dmsToDec(string $dms, string $hdg): float
     {
         return 0.0;
     }
 
     /**
-     * Decimal degrees to
+     * Decimaldegree to sexagesimal
      * @param float $dec
+     * @param int   $axis
      * @return string
      */
-    public static function decToDms(float $dec): string
+    public static function decToDms(float $dec, int $axis = self::LATITUDE): string
     {
-        return '';
+        $hdg = self::LATITUDE == $axis ? (0 <= $dec ? 'N' : 'S') : (0 <= $dec ? 'E' : 'W');
+        $dec = abs($dec);
+        $deg = floor($dec);
+        $min = floor(60 * ($dec - $deg));
+        $sec = 3600 * ($dec - $deg) - 60 * $min;
+        return sprintf('%d°%02d\'%s"%s', $deg, $min, str_pad(sprintf('%.1f', $sec), 4, '0', STR_PAD_LEFT), $hdg);
     }
 
     /**
-     * @param float $gps
+     * NMEA gps to decimaldegree
+     * @param string $gps
      * @param string $hdg
-     * @param int $decimals
+     * @param int    $decimals
      * @return float
      */
     public static function gpsToDec(string $gps, string $hdg, int $decimals = 6): float
