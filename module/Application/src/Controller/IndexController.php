00bfd23aa6862378d1c42b1d683cb4383978e810
Chris Raidler <chris@raidler.ch>
2020-03-03 11:38:00 +0100

    Roadbook

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends AbstractActionController
      */
     public function indexAction()
     {
-        return $this->redirect()->toRoute('roadbook');
+        return $this->redirect()->toRoute('logger');
     }
 
     /**
