2c50f5d4dcbee9670e7da515881587b4bd89489c
Chris Raidler <chris@raidler.ch>
2020-03-04 13:17:11 +0100

    Roadbook

--- module/Roadbook/src/Model/Position.php
+++ module/Roadbook/src/Model/Position.php
@@ -2,6 +2,7 @@
 namespace Roadbook\Model;
 
 use Application\Model\Item;
+use Application\Util\Coordinates;
 
 /**
  * Class Position
@@ -26,5 +27,24 @@ abstract class Position extends Item
     public function longitude()
     {
         return explode(',', $this->position)[1];
+    }
+
+    /**
+     * @return array
+     */
+    public function positionDD(): array
+    {
+        return explode(',', $this->position);
+    }
+
+    /**
+     * @return array
+     */
+    public function positionDMS(): array
+    {
+        return [
+            Coordinates::decToDms($this->latitude, Coordinates::LATITUDE),
+            Coordinates::decToDms($this->longitude, Coordinates::LONGITUDE),
+        ];
     }
 }
